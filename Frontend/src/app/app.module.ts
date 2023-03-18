0d7d605fd7e60e1a32c5f072a0301fae223d011f
DBiles <Darron.biles@gmail.com>
2023-03-18 13:22:13 -0400

    updates for user wallet - need lottery contract address

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,19 +1,19 @@
-import { OwnerInteractions } from '../components/ownerInteractions/ownerInteractions.component';
-import { LotteryInteractions } from '../components/lotteryInteractions/lotteryInteractions.component';
-import { ConnectedWalletInfo } from '../components/connectedWalletInfo/connectedWalletInfo.component';
-import { CardLotteryInfoComponent } from '../components/cardLotteryInfo/cardLotteryInfo.component';
+import { OwnerInteractions } from './../components/ownerInteractions/ownerInteractions.component';
+import { LotteryInteractions } from './../components/lotteryInteractions/lotteryInteractions.component';
+import { ConnectedWalletComponent } from './../components/connectedWalletInfo/connectedWalletInfo.component';
+import { CardLotteryInfoComponent } from './../components/cardLotteryInfo/cardLotteryInfo.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CardLotteryToken } from 'Frontend/src/components/cardLotteryToken/cardLotteryToken.component';
+import { CardLotteryToken } from 'src/components/cardLotteryToken/cardLotteryToken.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardLotteryInfoComponent,
     CardLotteryToken,
-    ConnectedWalletInfo,
+    ConnectedWalletComponent,
     LotteryInteractions,
     OwnerInteractions
   ],
