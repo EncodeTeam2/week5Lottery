5e47328c0a2dc28bed4d47ed05fac318daf6ca46
DBiles <Darron.biles@gmail.com>
2023-03-18 13:22:13 -0400

    updates for user wallet - need lottery contract address

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ConnectedWalletComponent } from '../components/connectedWalletInfo/connectedWalletInfo.component'
 
 @Component({
   selector: 'app-root',
