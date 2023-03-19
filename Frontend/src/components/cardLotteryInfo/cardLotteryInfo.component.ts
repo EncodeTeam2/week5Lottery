41e585985d19da35b3ba19eff718ea02c157b684
Carson <c_halter@live.ca>
2023-03-18 23:26:44 -0600

    add bets open and closing time to lottery card

--- Frontend/src/components/cardLotteryInfo/cardLotteryInfo.component.ts
+++ Frontend/src/components/cardLotteryInfo/cardLotteryInfo.component.ts
@@ -18,28 +18,47 @@ export class CardLotteryInfoComponent {
   purchaseRatio: number | undefined;
   betPrice: number | undefined;
   betFee: number | undefined;
+  betsOpen: boolean | undefined;
+  betsClosingTime: number | undefined;
 
   constructor() {
     this.provider = new ethers.providers.Web3Provider(window.ethereum, 'any');
   }
   async ngOnInit() {
-      this.lotteryAddress = LOTTERY_CONTRACT_ADDRESS;
-      this.lotteryContract = new Contract(
-        this.lotteryAddress,
-        lotteryJson.abi,
-        this.walletSigner ?? this.provider
-      );
-      this.lotteryContract['purchaseRatio']().then((purchaseRatioNum: number) => {
-        this.purchaseRatio = purchaseRatioNum;
-      });
-      this.lotteryContract['betPrice']().then((betPriceNum: number) => {
-        const betPriceStr = ethers.utils.formatEther(betPriceNum);
-        this.betPrice = parseFloat(betPriceStr);
-      });
-      this.lotteryContract['betFee']().then((betFeeNum: number) => {
-        const betFeeStr = ethers.utils.formatEther(betFeeNum);
-        this.betFee = parseFloat(betFeeStr);
-      });
-  }
+    // Connects to Lottery Contract
+    this.lotteryAddress = LOTTERY_CONTRACT_ADDRESS;
+    this.lotteryContract = new Contract(
+      this.lotteryAddress,
+      lotteryJson.abi,
+      this.walletSigner ?? this.provider
+    );
 
+    // Purchase Ratio Method
+    this.lotteryContract['purchaseRatio']().then((purchaseRatioNum: number) => {
+      this.purchaseRatio = purchaseRatioNum;
+    });
+
+    // Bet Price Method
+    this.lotteryContract['betPrice']().then((betPriceNum: number) => {
+      const betPriceStr = ethers.utils.formatEther(betPriceNum);
+      this.betPrice = parseFloat(betPriceStr);
+    });
+
+    // Bet Fee Method
+    this.lotteryContract['betFee']().then((betFeeNum: number) => {
+      const betFeeStr = ethers.utils.formatEther(betFeeNum);
+      this.betFee = parseFloat(betFeeStr);
+    });
+
+    // Are bets open?
+    this.lotteryContract['betsOpen']().then((isOpen: boolean) => {
+      this.betsOpen = isOpen;
+    });
+
+    // Bets closing time
+    this.lotteryContract['betsClosingTime']().then((closingTime: number) => {
+      const betsClosingTimeStr = ethers.utils.formatEther(closingTime);
+      this.betsClosingTime = parseFloat(betsClosingTimeStr);
+    });
+  }
 }
