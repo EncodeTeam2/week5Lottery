8753ca0b69c6a9c70dd18e186fa8908b971f76ba
DBiles <darron.biles@gmail.com>
2023-03-20 03:13:30 -0400

    Open bets, bet once, and updated to the correct contracts

--- Frontend/src/components/connectedWalletInfo/connectedWalletInfo.component.ts
+++ Frontend/src/components/connectedWalletInfo/connectedWalletInfo.component.ts
@@ -17,7 +17,6 @@ declare global {
   templateUrl: './connectedWalletInfo.component.html',
   providers: [],
 })
-
 export class ConnectedWalletComponent {
   walletSigner: ethers.providers.JsonRpcSigner | undefined;
   provider: ethers.providers.Web3Provider | undefined;
@@ -34,10 +33,10 @@ export class ConnectedWalletComponent {
   // Creates the Web3 Provider to send / sign transactions interacting with the blockchain
   constructor() {
     this.provider = new ethers.providers.Web3Provider(window.ethereum, 'any');
-    this.lotteryTokenContractAddress = '0x6354dA7FbCc203D49cEC938727A9A97a4c80bDE7';
     this.lotteryContractAddress = '0x6354dA7FbCc203D49cEC938727A9A97a4c80bDE7';
-    this.lotteryPrizeWinnings = "0";
-    this.lotteryTokenBalance = "0";
+    this.lotteryTokenContractAddress = '0x730369d165aBb8F72f01933Cfa858b5f269590cB';
+    this.lotteryPrizeWinnings = '0';
+    this.lotteryTokenBalance = '0';
   }
 
   // Get the ETH balance of the connected wallet
@@ -47,9 +46,9 @@ export class ConnectedWalletComponent {
         const balanceStr = ethers.utils.formatEther(balance);
         this.userEthBalance = balanceStr;
       })
-      .then(() => {
-        this.getLotteryTokenBalance(this.walletSigner!);
-        this.getLotteryPrizeWinnings();
+      .then(async () => {
+        await this.getLotteryTokenBalance(this.walletSigner!);
+        await this.getLotteryPrizeWinnings();
       })
       .catch((error: any) => {
         console.log(error);
@@ -62,6 +61,7 @@ export class ConnectedWalletComponent {
 
     await this.lotteryTokenContract['balanceOf'](this.walletSigner?._address)
       .then((balance: BigNumber) => {
+        console.log('Now getting lottery prize winnings');
         const tokenBalanceStr = ethers.utils.formatEther(balance);
         this.lotteryTokenBalance = tokenBalanceStr;
       })
@@ -137,9 +137,11 @@ export class ConnectedWalletComponent {
     this.lotteryContract = new Contract(this.lotteryContractAddress, lotteryABI, this.walletSigner);
     await this.lotteryContract
       .connect(this.walletSigner!)
-      ['purchaseTokens']({ value: ethers.utils.formatEther(value) }).then(() => {
-        console.log('Tokens purchased');
-        this.getWalletInfo();
+      ['purchaseTokens']({ value: ethers.utils.parseEther(value) })
+      .then(async () => {
+        console.log('Tokens purchased', value);
+        console.log('Tokens purchased', ethers.utils.parseEther(value));
+        await this.getWalletInfo();
       })
       .catch((error: any) => {
         if (error.code === 4001) {
@@ -151,9 +153,34 @@ export class ConnectedWalletComponent {
   }
 
   async getLotteryPrizeWinnings() {
-    this.lotteryContract  = new Contract(this.lotteryContractAddress, lotteryABI, this.walletSigner);
-    await this.lotteryContract['prize'](this.walletAddress).then((result: BigNumber) => {
-      this.lotteryPrizeWinnings = ethers.utils.formatEther((result));
+    this.lotteryContract = new Contract(this.lotteryContractAddress, lotteryABI, this.walletSigner);
+    await this.lotteryContract['prize'](this.walletAddress)
+      .then((result: BigNumber) => {
+        this.lotteryPrizeWinnings = ethers.utils.formatEther(result);
+      })
+      .catch((err: any) => {
+        console.log(err);
+      });
+  }
+
+  async placeBet() {
+    this.lotteryContract = new Contract(this.lotteryContractAddress, lotteryABI, this.walletSigner);
+    await this.lotteryContract['bet']()
+      .then(async (result: any) => {
+        console.log('we place a bet');
+      })
+      .catch((err: any) => {
+        return console.log('Bets are closed');
+      });
+  }
+
+  async openBets(duration: string) {
+    this.lotteryContract = new Contract(this.lotteryContractAddress, lotteryABI, this.walletSigner);
+    const currentBlock = await ethers.providers.getDefaultProvider().getBlock('latest');
+    await this.lotteryContract['openBets'](currentBlock.timestamp + Number(duration)).then((result: any) => {
+      console.log('Time of block closing', currentBlock.timestamp + Number(duration));
+      console.log('Bets are open');
+      console.log('result', result);
     }).catch((err: any) => {
       console.log(err);
     });
